74660ac923a46d4b7e7beeb31cc4ef52aa0437bc
Himanshu <himbaghel19@gmail.com>
2023-07-27 20:08:11 +0530

    Initial Commit:Learning redux toolkit

---
+++ index.js
@@ -0,0 +1,112 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+
+const CAKE_ORDER = "CAKE_ORDER";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDER,
+    payload: 1,
+  };
+}
+
+function restockedCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIcecream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+function restockedIcecream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+const initialIcecreamState = {
+  numOfIcecream: 20,
+};
+
+// (previousState,action)=> newState
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case CAKE_ORDER:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const IcecreamReducer = (state = initialIcecreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = redux.combineReducers({
+  cake: cakeReducer,
+  icecream: IcecreamReducer,
+});
+
+const store = createStore(rootReducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state", store.getState())
+);
+
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+
+// store.dispatch(restockedCake(3));
+
+const actions = bindActionCreators(
+  { orderCake, restockedCake, orderIcecream, restockedIcecream },
+  store.dispatch
+);
+
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockedCake(3);
+actions.orderIcecream();
+actions.orderIcecream();
+actions.restockedIcecream(2);
+
+unsubscribe();
---
+++ nested-state.js
@@ -0,0 +1,45 @@
+const redux = require("redux");
+const produce = require("immer").produce;
+const initialState = {
+  name: "himanshu",
+  address: {
+    street: "shinde ki chwani",
+    city: "gwalior",
+    state: "M.P",
+  },
+};
+
+const STREET_UPDATE = "STREET_UPDATE";
+
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATE,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATE:
+      //   return {
+      //     ...state,
+      //     address: {
+      //       ...state.address,
+      //       street: action.payload,
+      //     },
+      //   };
+      return produce(state, (draft) => {
+        draft.address.street = action.payload;
+      });
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
+store.dispatch(updateStreet("Vinay city"));
+unsubscribe();
